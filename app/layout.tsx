e5b968fc4d7d56911a1b74cacb0f853caa0360ac
chaudhary-hadi27 <chaudharyhadi27@gmail.com>
2025-11-18 20:19:06 +0500

    Favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Usman Fast Food - Digital Menu",
   description: "Browse our delicious menu and order your favorite food",
+
+  icons: {
+    icon: "/favicon.ico", // Normal browser favicon
+    apple: "/apple-touch-icon.png", // iPhone/iPad icon
+    shortcut: "/favicon.ico",
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
 };
 
 export default function RootLayout({
